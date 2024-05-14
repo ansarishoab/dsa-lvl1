71702179005ed1ec4e289930dbf3bf2ae802c618
ansarishoab <shoab.8055@gmail.com>
2024-05-14 23:47:15 +0530

    fix issue

--- src/e-9-lcm-gcd.java
+++ src/e-9-lcm-gcd.java
@@ -9,11 +9,11 @@ class PG9 {
         int on2 = n2;
         while(n1%n2 !=0){
             int r = n1%n2;
-            n2 = n1;
-            n1= r;
+            n1 = n2;
+            n2= r;
         }
         int gcd = n2;
         int lcd = (on1*on2)/gcd;
-        System.out.print("GCD: "+n2+ "LCD: "+ lcd);
+        System.out.print("GCD: "+n2+ "\nLCD: "+ lcd);
     }
 }
