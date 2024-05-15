913dc111a573369c2c9314745368c331319f9964
ansarishoab <shoab.8055@gmail.com>
2024-05-16 00:59:06 +0530

    day-4 excercise

--- src/e-10-prime-factorization.java
+++ src/e-10-prime-factorization.java
@@ -4,13 +4,26 @@ class PG10 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int factor = 2;
-        while (n > 1) {
-            while (n % factor == 0) {
-                System.out.println(factor);
-                n = n / factor;
+        /* approach one */
+        // int factor = 2;
+        // while (n > 1) {
+        // while (n % factor == 0) {
+        // System.out.println(factor);
+        // n = n / factor;
+        // }
+        // factor++;
+        // }
+
+        /* approach two */
+        for (int div = 2; div < n; div++) {
+            while (n % div == 0) {
+                n = n / div;
+                System.out.println(div);
             }
-            factor++;
         }
+        if(n!=1){
+            System.out.println(n);
+        }
+
     }
 }
